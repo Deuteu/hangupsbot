8473e375f76ab090d30a7108506fdac0634982a9
Deuteu <Deuteu@users.noreply.github.com>
2015-04-19 21:47:39 +0200

    Champagne : Delete unused code, Rename variable

--- hangupsbot/plugins/champagne.py
+++ hangupsbot/plugins/champagne.py
@@ -21,26 +21,9 @@ def _initialise(command, bot=None):
     return []
 
 def _create_champagne_in_memory(bot):
-    cd = 4*3600
-    max = 4
-    # migrate DND list to memory.json
-    champagne = {}
-    bot.memory.set_by_path([keyword], champagne)
+    dict = {}
+    bot.memory.set_by_path([keyword], dict)
     bot.memory.save()
-
-    # migrate memory.json DND to structure with more metadata
-    if bot.memory.exists(["donotdisturb"]):
-        donotdisturb = bot.memory.get("donotdisturb")
-        if(isinstance(donotdisturb, list)):
-            # legacy structure, convert to dict
-            dnd_dict = {}
-            for user_id in donotdisturb:
-                dnd_dict[user_id] = {
-                    "created": time.time(),
-                    "expiry": 86400
-                }
-            bot.memory.set_by_path(["donotdisturb"], dnd_dict)
-            bot.memory.save()
 
 @asyncio.coroutine
 def _handle_autoreply(bot, event, command):
@@ -65,12 +48,12 @@ def champagne_in_text(text):
 
 def is_burn(bot, event):
     """"Check number of times this autoreply was used and limit it"""
-    champagne = bot.memory.get(keyword)
+    dict = bot.memory.get(keyword)
     conv_id = event.conv.id_
-    if conv_id in champagne:
-        hacks = champagne[conv_id]["hack"]
+    if conv_id in dict:
+        hacks = dict[conv_id]["hack"]
         if hacks >= times:
-            if champagne[conv_id]["timer"] + cooldown > time.time():
+            if dict[conv_id]["timer"] + cooldown > time.time():
                 return True
             else:
                 heat_sink(bot, conv_id)
@@ -95,13 +78,13 @@ def heat_sink(bot, conv_id):
         "timer": time.time(),
         "hack": 0
     }
-    champagne = bot.memory.get(keyword)
-    champagne[conv_id] = hack_status
-    bot.memory.set_by_path([keyword], champagne)
+    dict = bot.memory.get(keyword)
+    dict[conv_id] = hack_status
+    bot.memory.set_by_path([keyword], dict)
     bot.memory.save()
 
 def hack(bot, conv_id):
-    champagne = bot.memory.get(keyword)
-    champagne[conv_id]["hack"] = champagne[conv_id]["hack"] + 1
-    bot.memory.set_by_path([keyword], champagne)
+    dict = bot.memory.get(keyword)
+    dict[conv_id]["hack"] = dict[conv_id]["hack"] + 1
+    bot.memory.set_by_path([keyword], dict)
     bot.memory.save()
